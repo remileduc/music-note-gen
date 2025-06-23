47cadf4355a3240dc3d66d6d933f0c3fa691aec2
Rémi Ducceschi <remi.ducceschi@gmail.com>
2025-06-23 19:14:46 +0200

    Buil in export mode for future github pages

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  images: {
-	formats: ["image/avif", "image/webp"]
-  }
+	output: "export",
+	images: {
+		unoptimized: true, // because of output export
+		formats: ["image/avif", "image/webp"]
+	}
 };
 
 export default nextConfig;
---
+++ src/app/manifest.ts
@@ -0,0 +1,35 @@
+import type { MetadataRoute } from "next"
+
+export const dynamic = 'force-static';
+
+export default function manifest(): MetadataRoute.Manifest
+{
+	return {
+		name: "Générateur de notes de musique",
+		short_name: "Musique Gen",
+		description: "Ce projet consiste à mettre à disposition un générateur de notes de musique pour entrainement débutant",
+		lang: "fr",
+		start_url: "/",
+		display: "standalone",
+		orientation: "portrait-primary",
+		background_color: "#e6f0ff",
+		theme_color: "#602353",
+		icons: [
+			{
+				src: "/violin.svg",
+				sizes: "any",
+				type: "image/svg+xml",
+			},
+			{
+				src: "/violin.256x256.png",
+				sizes: "256x256",
+				type: "image/png",
+			},
+			{
+				src: "/violin.512x512.png",
+				sizes: "512x512",
+				type: "image/png",
+			}
+		],
+	}
+}
