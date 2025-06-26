abc815f87284227bdaf1315a751083b6f6de64e3
Rémi Ducceschi <remi.ducceschi@gmail.com>
2025-06-27 00:25:56 +0200

    Fix links in manifest

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -1,3 +1,4 @@
+import { rsrcPath } from "@utils/global";
 import type { MetadataRoute } from "next"
 
 export const dynamic = 'force-static';
@@ -9,24 +10,24 @@ export default function manifest(): MetadataRoute.Manifest
 		short_name: "Musique Note Gen",
 		description: "Ce projet consiste à mettre à disposition un générateur de notes de musique pour entrainement débutant",
 		lang: "fr",
-		start_url: "/",
+		start_url: rsrcPath("/"),
 		display: "standalone",
 		orientation: "portrait-primary",
 		background_color: "#e6f0ff",
 		theme_color: "#602353",
 		icons: [
 			{
-				src: "/violin.svg",
+				src: rsrcPath("/violin.svg"),
 				sizes: "any",
 				type: "image/svg+xml",
 			},
 			{
-				src: "/violin.256x256.png",
+				src: rsrcPath("/violin.256x256.png"),
 				sizes: "256x256",
 				type: "image/png",
 			},
 			{
-				src: "/violin.512x512.png",
+				src: rsrcPath("/violin.512x512.png"),
 				sizes: "512x512",
 				type: "image/png",
 			}
