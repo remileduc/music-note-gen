8050a7c749adb62393461c37e39c3582bc5b9555
Rémi Ducceschi <remi.ducceschi@gmail.com>
2025-07-19 21:43:46 +0200

    Add tests for validator

--- tests/Note.test.ts
+++ tests/Note.test.ts
@@ -1,12 +1,12 @@
 import type { Accidental as VAccidental } from "vexflow";
-import { Note } from "../src/utils/Note"
+import { type FrenchNoteName, Note } from "../src/utils/Note"
 import { MockFactory } from "./utils";
 
 describe("Note", () => {
 	describe("default constructor", () => {
-		const note = new Note();
 
 		it("should use default constructor properties", () => {
+			const note = new Note();
 			expect(note.name).toBe("do");
 			expect(note.nameEN).toBe("c");
 			expect(note.duration).toBe("q");
@@ -16,22 +16,31 @@ describe("Note", () => {
 	});
 
 	describe("parameterized constructor", () => {
-		const note1 = new Note({name: "fa", octave: 3, mod: "b", duration: "8"});
 		it("should be initialized correctly", () => {
-			expect(note1.name).toBe("fa");
-			expect(note1.nameEN).toBe("f");
-			expect(note1.duration).toBe("8");
-			expect(note1.mod).toBe("b");
-			expect(note1.octave).toBe(3);
+			const note = new Note({name: "fa", octave: 3, mod: "b", duration: "8"});
+			expect(note.name).toBe("fa");
+			expect(note.nameEN).toBe("f");
+			expect(note.duration).toBe("8");
+			expect(note.mod).toBe("b");
+			expect(note.octave).toBe(3);
 		});
 
-		const note2 = new Note({name: "mi", octave: 5, mod: "#", duration: "w"});
 		it("should be initialized correctly", () => {
-			expect(note2.name).toBe("mi");
-			expect(note2.nameEN).toBe("e");
-			expect(note2.duration).toBe("w");
-			expect(note2.mod).toBe("#");
-			expect(note2.octave).toBe(5);
+			const note = new Note({name: "mi", octave: 5, mod: "#", duration: "w"});
+			expect(note.name).toBe("mi");
+			expect(note.nameEN).toBe("e");
+			expect(note.duration).toBe("w");
+			expect(note.mod).toBe("#");
+			expect(note.octave).toBe(5);
+		});
+
+		it("should initialize correctly missing / wrong parameters", () => {
+			const note = new Note({name: "lol" as FrenchNoteName, octave: 5, mod: "#"});
+			expect(note.name).toBe("do");
+			expect(note.nameEN).toBe("c");
+			expect(note.duration).toBe("q");
+			expect(note.mod).toBe("#");
+			expect(note.octave).toBe(5);
 		});
 	});
 
@@ -44,12 +53,16 @@ describe("Note", () => {
 	});
 
 	describe("toString", () => {
-		const note1 = new Note({name: "ré", octave: 3, mod: "b", duration: "8"});
-		const note2 = new Note({name: "ré", octave: 3, mod: "", duration: "8"});
 
 		it("should print correct string", () => {
+			const note1 = new Note({name: "ré", octave: 3, mod: "b", duration: "8"});
+			const note2 = new Note({name: "ré", octave: 3, mod: "", duration: "8"});
 			expect(note1.toString()).toBe("ré b (3)");
 			expect(note2.toString()).toBe("ré (3)");
+		});
+		it("should print nothing for silences", () => {
+			const note = new Note({name: "ré", octave: 3, mod: "", duration: "qr"});
+			expect(note.toString()).toBe("");
 		});
 	});
 
