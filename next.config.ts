2c3a0bc40fe5d6753b06542a3542e2db407b8c4c
Rémi Ducceschi <remi.ducceschi@gmail.com>
2025-11-17 15:19:19 +0100

    Global: Update to NextJS 16

--- next.config.ts
+++ next.config.ts
@@ -3,9 +3,13 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
 	output: "export",
 	basePath: process.env.NEXT_PUBLIC_BASEPATH,
+	reactCompiler: true,
 	images: {
 		unoptimized: true, // because of output export
 		formats: ["image/avif", "image/webp"]
+	},
+	experimental: {
+		turbopackFileSystemCacheForDev: true
 	}
 };
 
